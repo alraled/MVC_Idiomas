21a3fa5c4e6b8c9c738c42b27a1da225578c32d2
alvaro <alraled@hotmail.es>
2016-02-23 09:58:43 +0100

    MVC_Idiomas

---
+++ MVC_Idiomas/Models/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Security.AccessControl;
+using System.Web.Management;
+
+namespace MVC_Idiomas.Models
+{
+    public class Persona
+    {
+
+        [Display (ResourceType = typeof(Persona),Name = "nombre")]
+        [Required(ErrorMessageResourceType = typeof(Persona), ErrorMessageResourceName = "nombre_o")]
+
+
+        public String Nombre { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Persona), ErrorMessageResourceName = "saldo_o")]
+        [DataType(DataType.Currency, ErrorMessageResourceType = typeof(Persona), ErrorMessageResourceName = "saldo_c")]
+        [Display(ResourceType = typeof(Persona), Name = "saldo")]
+
+        public Double Saldo { get; set; }
+    }
+}
